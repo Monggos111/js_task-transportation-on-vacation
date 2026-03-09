4b9ac7a1583ce95e5a05d912bb1f511f6471bc63
Vladimir Kolosov <12vova1212ukr@gmail.com>
2026-03-09 20:47:17 +0200

    add task solution 2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,12 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let total = days * 40;
+  const DAILY_RENTAL_COST = 40;
+  const LONG_TERM_DAYS = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const MEDIUM_TERM_DAYS = 3;
+  const MEDIUM_TERM_DISCOUNT = 20;
 
-  if (days >= 7) {
-    total -= 50;
-  } else if (days >= 3) {
-    total -= 20;
+  let total = DAILY_RENTAL_COST * 40;
+
+  if (days >= LONG_TERM_DAYS) {
+    total -= LONG_TERM_DISCOUNT;
+  } else if (days >= MEDIUM_TERM_DAYS) {
+    total -= MEDIUM_TERM_DISCOUNT;
   }
 
   return total;
